c8079af1671ea42d67e6b3af7c1767959014a854
Dmytro Smakovskyi <dmsmakovskyi@gmail.com>
2020-07-28 12:03:13 +0300

    in progress

--- src/Pages/homePage/HomePage.js
+++ src/Pages/homePage/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import AuthForm from '../../components/authForm/AuthForm'
 import styles from './homePage.module.css'
 
 const HomePage = () => {
@@ -8,8 +9,8 @@ const HomePage = () => {
           <div className={styles.wrapperDescription}>
             <h1 className={styles.title}>Kapusta</h1>
             <p className={styles.description}>smart finance</p>
-
           </div>
+          <AuthForm/>
         </div>
       </section>
     );
---
+++ src/components/authForm/AuthForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class AuthForm extends Component {
+  state = {  }
+  render() {
+    return (
+      <>authForm</>
+    );
+  }
+}
+
+export default AuthForm;
