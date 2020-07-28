4ba163730d4b1ca16011bdaa8b18627adb95c9b2
Dmytro Smakovskyi <dmsmakovskyi@gmail.com>
2020-07-28 16:17:51 +0300

    hp

--- src/Pages/homePage/HomePage.js
+++ src/Pages/homePage/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ReactComponent as CabbageIcon } from '../../ui/cabbage-icon.svg';
 import AuthForm from '../../components/authForm/AuthForm'
 import styles from './homePage.module.css'
 
@@ -6,8 +7,9 @@ const HomePage = () => {
     return (
       <section className={styles.sectionHomePage}>
         <div className={styles.wrapper}>
+            <CabbageIcon className={styles.icon}/>
           <div className={styles.wrapperDescription}>
-            <h1 className={styles.title}>Kapusta</h1>
+            <h1 className={styles.title}>Kapu$ta</h1>
             <p className={styles.description}>smart finance</p>
           </div>
           <AuthForm/>
--- src/components/authForm/AuthForm.js
+++ src/components/authForm/AuthForm.js
@@ -1,10 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import styles from './authForm.module.css'
 
 class AuthForm extends Component {
-  state = {  }
+  state = {
+    email: ""
+  }
   render() {
     return (
-      <>authForm</>
+      <div className={styles.authWrapper}>
+        <form>
+          <p className={styles.googleDescr}>Вы можете авторизироваться с помощью  google account:</p>
+          <Link className={styles.google} href="#">Google</Link>
+          <p className={styles.authDescr}>Или зайти в приложение с помощью имейла и пароля, сперва зарегистрировавшись:</p>
+          <label>
+          Email
+          <input
+            type="email"
+            placeholder="your@email.com"
+            name="email"
+            value=""
+            onChange={()=>{}}
+          />
+        </label>
+        <label>
+        Password
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            value=""
+            onChange={()=>{}}
+          />
+        </label>
+
+        <button type="submit">Sign up as "login"</button>
+        </form>
+      </div>
     );
   }
 }
