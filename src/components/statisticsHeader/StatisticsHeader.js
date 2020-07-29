b57506319f16a8f7529517c90db4b2e7abc8e9cf
Pedko777 <yuriipedko@gmail.com>
2020-07-29 16:54:21 +0300

    Add styles

--- src/components/statisticsHeader/StatisticsHeader.js
+++ src/components/statisticsHeader/StatisticsHeader.js
@@ -5,7 +5,7 @@ import { ReactComponent as ArrowBack } from './arrowBack/back.svg';
 import { ReactComponent as PrevMonth } from './arrowBack/leftArrow.svg';
 import { ReactComponent as NextMonth } from './arrowBack/rightArrow.svg';
 import styles from './StatisticsHeader.module.css';
-// import Media from 'react-media';
+import Media from 'react-media';
 
 class StatisticsHeader extends Component {
   state = {
@@ -48,47 +48,53 @@ class StatisticsHeader extends Component {
     // console.log(this.state.date);
     const { date } = this.state;
     return (
-      <div  className={`${styles.statisticsHeader} container`}>
-        <h2>Statictics header</h2>
-
-        <button
-          className={styles.button}
-          type="button"
-          onClick={this.handleGoBack}
-        >
-          <ArrowBack />
-          На главную
-        </button>
-        <div>
-          <p>Текущий период</p>
-          <div className={styles.calendarContainer}>
-            <button
-              className={styles.button}
-              type="button"
-              name="prevMonthBtn"
-              onClick={this.handleChangeMonth}
-            >
-              <PrevMonth />
-            </button>
-            <p className={styles.calendarText}>
-              {date && moment(date).format(' MMMM YYYY')}
-            </p>
-            <button
-              className={styles.button}
-              type="button"
-              name="nextMonthBtn"
-              onClick={this.handleChangeMonth}
-            >
-              <NextMonth />
-            </button>
-          </div>
+      <div className={`${styles.statisticsHeader} container`}>
+        <div className={styles.buttonContainer}>
+          <button
+            className={styles.button}
+            type="button"
+            onClick={this.handleGoBack}
+          >
+            <ArrowBack />
+          </button>
+          <Media query="(min-width: 768px)">
+            {matches => (matches ? <p>На главную</p> : <p></p>)}
+          </Media>
         </div>
-        <div>
-          <p>
-            Баланс на <span>{date && moment(date).format('L')}:</span>
-          </p>
-          <div className={styles.statisticsHeaderBalance}>
-            <span className={styles.statisticsSpan}>1111 UAH</span>
+        <div className={styles.miniContainer}>
+          <div className={styles.calendarWrapper}>
+            <Media query="(min-width: 768px)">
+              {matches => (matches ? <p>Текущий период</p> : <p></p>)}
+            </Media>
+            <div className={styles.calendarContainer}>
+              <button
+                className={styles.button}
+                type="button"
+                name="prevMonthBtn"
+                onClick={this.handleChangeMonth}
+              >
+                <PrevMonth />
+              </button>
+              <p className={styles.calendarText}>
+                {date && moment(date).format(' MMMM YYYY')}
+              </p>
+              <button
+                className={styles.button}
+                type="button"
+                name="nextMonthBtn"
+                onClick={this.handleChangeMonth}
+              >
+                <NextMonth />
+              </button>
+            </div>
+          </div>
+          <div className={styles.balanceContainer}>
+            <p className={styles.balance}>
+              Баланс на <span>{date && moment(date).format('L')}:</span>
+            </p>
+            <div className={styles.statisticsHeaderBalance}>
+              <span className={styles.statisticsSpan}>1111 UAH</span>
+            </div>
           </div>
         </div>
       </div>
