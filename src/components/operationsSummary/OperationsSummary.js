5c502294a4391772fa47aced3b6a1ef6a9eca5b1
ZavadskiyAS <zavadskiyweb@gmail.com>
2020-07-30 16:15:33 +0300

    start operations Summary

---
+++ src/components/operationsSummary/OperationsSummary.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import s from './OperationsSummary.module.css';
+
+const OperationsSummary = props => {
+    // const {data , cost} = props
+
+return (
+    <div className={s.table}>
+        <h2 className={s.tableHead}>Сводка</h2>
+        
+    </div>
+)
+}
