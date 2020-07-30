efd5b100aa88d6383aed0100bc144425df1c5084
Mavr1 <mavrodiy.m.e@gmail.com>
2020-07-30 18:30:35 +0300

    authOperations done

--- src/redux/operations/authOperations.js
+++ src/redux/operations/authOperations.js
@@ -2,35 +2,11 @@ import api from '../../services/api';
 import authSlice from '../auth/authSlice';
 import loaderSlice from '../loader/loaderSlice';
 
-// const registerResponse = {
-//   status: 'success',
-//   user: {
-//     userData: {
-//       name: {
-//         fullName: 'string',
-//         firstName: 'string',
-//         lastName: 'string',
-//       },
-//       email: 'user@example.com',
-//       photo: 'string',
-//       userNew: true,
-//     },
-//     token:
-//       'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZDNhMTM4NmIxZTg1NTdjZjIzNjY3ODEiLCJpYXQiOjE1NjQwODcxNzV9.jSdzHuBSf4yKS6t7zwt0AoQIchHlz73JDOjfHVdbTBk',
-//   },
-// };
-
-//{
-//   "status": "error",
-//   "error": "Server selection timed out after 30000 ms"
-// }
-
 export const register = (credentials) => (dispatch) => {
   dispatch(loaderSlice.actions.setLoadingTrue());
-  // console.log(credentials)
   api
     .register(credentials)
-    .then(({data}) => {
+    .then(({ data }) => {
       api.token.set(data.user.token);
       dispatch(authSlice.actions.registerSuccess(data.user));
       dispatch(authSlice.actions.clearError());
