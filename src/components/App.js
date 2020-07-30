b68026ab18907b31ad2d94808cbe5093650e3a4b
Dmytro Smakovskyi <dmsmakovskyi@gmail.com>
2020-07-30 13:29:10 +0300

    authForm logic in progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,30 @@
 import React, { Suspense } from "react";
-import { BrowserRouter, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
 import routes from "../routes";
     
 
-import PrivateRoute from "../services/PrivateRoute";
-import PublicRoute from "../services/PublicRoute";
+// import PrivateRoute from "../services/PrivateRoute";
+// import PublicRoute from "../services/PublicRoute";
 
-import Modal from "../components/modal/Modal";
-import OperationList from "./operationList/OperationList";
-import Header from "./header/Header";
-import ContactsPage from '../Pages/teamPage/TeamPage';
-import HomePage from '../Pages/homePage/HomePage'
+// import Modal from "../components/modal/Modal";
+// import OperationList from "./operationList/OperationList";
+// import Header from "./header/Header";
+// import ContactsPage from '../Pages/teamPage/TeamPage';
+import HomePage from '../Pages/homePage/HomePage';
 
-import PrivateRoute from "../services/PrivateRoute";
-import PublicRoute from "../services/PublicRoute";
+// import PrivateRoute from "../services/PrivateRoute";
+// import PublicRoute from "../services/PublicRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
+      {/* <Header /> */}
       <Suspense fallback={<h1>Loading...</h1>}>
           <Switch>
             
-            <Route  path="/contacts" component={ContactsPage}/>
+            {/* <Route  path="/contacts" component={ContactsPage}/> */}
 </Switch>
+            <Route  path="/" component={HomePage}/>
         {/* <Switch>
           {routes.map(route => {
             return route.private ? (
@@ -39,7 +40,7 @@ const App = () => {
           <Redirect to="/login" />
         </Switch> */}
       </Suspense>
-      <OperationList/>
+      {/* <OperationList/> */}
     </BrowserRouter>
   );
 };
--- src/components/authForm/AuthForm.js
+++ src/components/authForm/AuthForm.js
@@ -1,68 +1,119 @@
-import React, { Component } from "react";
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 import googleIcon from "../../ui/google-icon.png";
 import styles from "./authForm.module.css";
 
-class AuthForm extends Component {
-  state = {
-    email: "",
-  };
-  render() {
+const AuthForm = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [typeRegister, setTypeRegister] = useState(false);
+  
+  const handleInputFirstName = (e) => {e.preventDefault(); setFirstName(e.target.value)}
+  const handleInputLastName = (e) => {e.preventDefault(); setLastName(e.target.value)}
+  const handleInputEmail = (e) => {e.preventDefault(); setEmail(e.target.value)}
+  const handleInputPassword = (e) => {e.preventDefault(); setPassword(e.target.value)}
+  const handleTypeRegister = (e) => {e.preventDefault(); setTypeRegister(currentState=> {if(currentState){
+    return false;
+  } else {
+    return true;
+  }
+})}
+  
+
     return (
       <div className={styles.authWrapper}>
-        <form>
-          <p className={styles.googleDescr}>
-            Вы можете авторизироваться с помощью Google account:
-          </p>
-          <Link className={styles.google} href="#">
-            <img
-              className={styles.googleIcon}
-              src={googleIcon}
-              alt="google-icon"
-            />
-            Google
-          </Link>
-          <p className={styles.authDescr}>
-            Или зайти в приложение с помощью имейла и пароля, сперва
-            зарегистрировавшись:
-          </p>
-          <label className={styles.label} for="email">
-            Электронная почта
-          </label>
-          <input
-            id="email"
-            className={styles.input}
-            type="email"
-            placeholder="Your@email.com"
-            name="email"
-            value=""
-            onChange={() => {}}
-          />
-          <label className={styles.label} for="password">
-            Пароль
-          </label>
-          <input
-            id="password"
-            className={styles.input + " " + styles.inputPassword}
-            type="password"
-            placeholder="Пароль"
-            name="password"
-            value=""
-            onChange={() => {}}
-          />
+       <form onSubmit={()=>{}}>
+  
+  <p className={styles.googleDescr}>
+    Вы можете авторизироваться с помощью Google account:
+  </p>
+  <Link className={styles.google} href="#">
+    <img
+      className={styles.googleIcon}
+      src={googleIcon}
+      alt="google-icon"
+    />
+    Google
+  </Link>
+  <p className={styles.authDescr}>
+    Или зайти в приложение с помощью имейла и пароля, сперва
+    зарегистрировавшись:
+  </p>
 
-          <div className={styles.authBtnWrapper}>
-            <button className={styles.buttonLogin} type="submit">
-              войти
-            </button>
-            <button className={styles.buttonRegister} type="submit">
-              регистрация
-            </button>
-          </div>
-        </form>
+  {typeRegister && 
+  <>
+  <label className={styles.label} for="name">
+    Имя
+  </label>
+
+  <input
+    id="name"
+    className={styles.input}
+    type="text"
+    placeholder="First name"
+    name="firstName"
+    value={firstName}
+    onChange={handleInputFirstName}
+  />
+
+  <label className={styles.label} for="lastName">
+  Фамилия
+  </label>
+
+  <input
+    id="lastName"
+    className={styles.input}
+    type="text"
+    placeholder="Last name"
+    name="lastName"
+    value={lastName}
+    onChange={handleInputLastName}
+  />
+  </>}
+
+  <label className={styles.label} for="email">
+    Электронная почта
+  </label>
+
+  <input
+    id="email"
+    className={styles.input}
+    type="email"
+    placeholder="Your@email.com"
+    name="email"
+    value={email}
+    onChange={handleInputEmail}
+  />
+  <label className={styles.label} for="password">
+    Пароль
+  </label>
+
+  <input
+    id="password"
+    className={styles.input + " " + styles.inputPassword}
+    type="password"
+    placeholder="Пароль"
+    name="password"
+    value={password}
+    onChange={handleInputPassword}
+  />
+
+  <div className={styles.authBtnWrapper}>
+    <button className={styles.buttonLogin} type="submit">
+      войти
+    </button>
+    <button className={styles.buttonRegister} type="submit" onClick={handleTypeRegister}>
+      {typeRegister ? 'логинизация' : 'регистрация'}
+    </button>
+  </div>
+
+</form>
       </div>
     );
   }
-}
+
 
 export default AuthForm;
+  
