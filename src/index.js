e58d2332af23b94f8c176232f134d8c18d0fc1be
hrushkooleksii <hrushkooleksii@gmail.com>
2020-07-30 09:49:45 +0300

    adding import for fonts and variables

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import './normalize.css';
+import './services/fonts.css';
+import './services/variables.css';
 import './index.css';
 import App from './components/App';
 import { store, persistor } from './redux/store';
