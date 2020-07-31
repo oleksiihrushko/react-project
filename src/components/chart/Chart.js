50bc1fc83ac3b7f25bf9ff3350b031c449692e56
ISvirko <ISvirko@yahoo.com>
2020-07-31 10:54:20 +0300

    initial chart

---
+++ src/components/chart/Chart.js
@@ -0,0 +1,11 @@
+import React from "react";
+import BarChart from "./BarChart";
+import HorizontalChart from "./horizontalChart";
+import { useWindowWidth } from "./hooks";
+
+const Chart = () => {
+  const width = useWindowWidth();
+  return width > 320 ? <BarChart /> : <HorizontalChart />;
+};
+
+export default Chart;
