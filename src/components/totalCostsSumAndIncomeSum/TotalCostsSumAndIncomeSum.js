7dbcfff2fe7c30d0838972590f41da6fd788ec0f
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-08-01 22:29:44 +0300

    fix f

--- src/components/totalCostsSumAndIncomeSum/TotalCostsSumAndIncomeSum.js
+++ src/components/totalCostsSumAndIncomeSum/TotalCostsSumAndIncomeSum.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import s from './TotalCostsSumAndIncomeSum.module.css';
 import { useSelector } from 'react-redux';
-
+import moment from 'moment';
 
 
 function TotalCostsSumAndIncomeSum() {
@@ -10,9 +10,52 @@ function TotalCostsSumAndIncomeSum() {
   const income = useSelector(state => state.operations.income);
   const month = useSelector(state => state.statistics.month);
   console.log(month);
-
-    return (
-      <div className={"container"}>
+  const {
+    date,
+    selectedCategory,
+    categoriesList,
+  } = this.state;
+  let chartData = null;
+  let categoryList = [];
+  let costsSum = 0;
+  if (costs && costs.length > 0) {
+    costsSum = costs
+        .filter(
+          item =>
+            moment(item.date).format('MMMM YYYY') ===
+            moment(date).format('MMMM YYYY'),
+        )
+        .reduce((acc, el) => acc + el.amount, 0);
+    }
+    
+    let incomeSum = 0;
+    if (income && income.length > 0) {
+      incomeSum = income
+      .filter(
+        item =>
+        moment(item.date).format('MMMM YYYY') ===
+        moment(date).format('MMMM YYYY'),
+        )
+        .reduce((acc, el) => acc + el.amount, 0);
+      }
+      
+      if (costs && costs.length > 0) {
+        categoryList = this.costs;
+        if (selectedCategory === '') {
+          chartData = this.categoryList;
+        } else {
+          chartData = this.costs;
+        }
+      } else if (categoriesList && categoriesList.length > 0) {
+        categoryList = categoriesList.map(category => ({
+          category: category.name,
+          amount: 0,
+          id: category.name,
+        }));
+      }
+      
+      return (
+        <div className={"container"}>
         <div className={s.wrapper}>
           <div className={s.column}>
             <p className={s.stat_title}>Расходы:</p>
@@ -33,4 +76,4 @@ function TotalCostsSumAndIncomeSum() {
     incomeSum: PropTypes.number.isRequired,
   };
 
-  export default TotalCostsSumAndIncomeSum;
+    export default TotalCostsSumAndIncomeSum;
