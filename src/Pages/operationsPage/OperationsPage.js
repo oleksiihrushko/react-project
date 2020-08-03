0f6ec4a82a7eb9195a031e82e0fd7bd14524b19f
unknown <you@example.com>
2020-08-03 12:24:44 +0300

    update

--- src/Pages/operationsPage/OperationsPage.js
+++ src/Pages/operationsPage/OperationsPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import OpreationsHeader from "../../components/operationsHeader/OperationsHeader";
-import OpreationsHeader from "../../components/addOperationForm/AddOperationForm";
-import OpreationsHeader from "../../components/operationList/OperationList";
+import AddOperationForm from "../../components/addOperationForm/AddOperationForm";
+import OperationList from "../../components/operationList/OperationList";
 const OperationsPage = () => {
   return (
     <div>
