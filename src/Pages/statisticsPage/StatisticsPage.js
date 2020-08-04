5c30286fc33ae84bf382b491271a54c9c8dcd453
ZavadskiyAS <zavadskiyweb@gmail.com>
2020-08-04 15:52:30 +0300

    st 2

--- src/Pages/statisticsPage/StatisticsPage.js
+++ src/Pages/statisticsPage/StatisticsPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import StatistcsHeader from '../../components/statisticsHeader/StatisticsHeader';
 import TotalCostsSumAndIncomeSum from '../../components/totalCostsSumAndIncomeSum/TotalCostsSumAndIncomeSum';
