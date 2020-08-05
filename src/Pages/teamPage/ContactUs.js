9d6220c25f4de9d8df50a276844a8b507252cee5
ZavadskiyAS <zavadskiyweb@gmail.com>
2020-08-05 10:46:21 +0300

    new Team page

--- src/Pages/teamPage/ContactUs.js
+++ src/Pages/teamPage/ContactUs.js
@@ -1,33 +1,28 @@
 import React from 'react';
 import s from './ContactUs.module.css';
-// import s from './TeamPageMobail.module.css';
-import photo1 from './pageContactUs/photo1.jpg';
-import photo2 from './pageContactUs/photo2.jpg';
-import photo3 from './pageContactUs/photo3.jpg';
-import photo4 from './pageContactUs/photo4.jpg';
-import photo5 from './pageContactUs/photo5.jpg';
-import photo6 from './pageContactUs/photo6.jpg';
-import photo7 from './pageContactUs/photo7.jpg';
-import photo8 from './pageContactUs/photo8.jpg';
-import photo9 from './pageContactUs/photo9.jpg';
-import photo10 from './pageContactUs/photo10.jpg';
+import noImage from './pageContactUs/noFace.jpg';
+import teamList from '../teamPage/teamList';
 
 const ContactUs = () => {
     return (
-        <div className={`${s.contactus_div_main} container`}>
-            <ul className={s.contactus_ul}>
-                <li className={s.card}>
-                    <div className={s.imgBox}>
-                        <img src={photo1} alt="us" width="300" className={s.contactus_img} />
-                    </div>
-                    <div className={s.text}>
-                        <p className={s.contactus_name_p}>asfsadffa</p>
-                        <p className={s.contactus_title_p}>Team Lead</p>
-                        <p className={s.contactus_email_p}>afsdfs@gmail.com</p>
-                    </div>
-                </li>
-            </ul>
-        </div>
+        <ul className={s.lcontainer}>
+            {teamList.map(contact => {
+                return (
+                    <li className={s.bgamecard}>
+                        <div className={s.bgamecard__cover}>
+                            {/* <img src = {contact.image ? contact.photo : noImage} */}
+                            < img src={require(contact.photo)}
+                            alt="us"
+                            width = "300"
+                           />
+                            <p>{contact.firstName}</p>
+                            <p>{contact.lastName}</p>
+                            <p>{contact.email}</p>
+                        </div>
+                    </li>)
+        })}
+            
+        </ul>
     );
 };
 
---
+++ src/Pages/teamPage/teamList.js
@@ -0,0 +1,75 @@
+// import photo from './pageContactUs/photo1.jpg';
+
+const teamList = [{
+    firstName: 'Александр',
+    lastName: 'Завадский',
+    email: 'Zavadskiyweb@gmail.com',
+    photo: './pageContactUs/photo1.jpg',
+    linkedIn: '',
+},
+{
+    firstName: 'Дмитрий',
+    lastName: 'Смаковский',
+    email: 'dmsmakovskyi@gmail.com',
+    photo: '',
+    linkedIn: '',
+},
+{
+    firstName: 'Юрий',
+    lastName: 'Педько',
+    email: 'yuriipedko@gmail.com',
+    photo: '',
+    linkedIn: '',
+},
+{
+    firstName: '',
+    lastName: '',
+    email: '',
+    photo: '',
+    linkedIn: '',
+    
+},
+{
+    firstName: '',
+    lastName: '',
+    email: '',
+    photo: '',
+    linkedIn: '',
+},
+{
+    firstName: '',
+    lastName: '',
+    email: '',
+    photo: '',
+    linkedIn: '',
+},
+{
+    firstName: '',
+    lastName: '',
+    email: '',
+    photo: '',
+    linkedIn: '',
+},
+{
+    firstName: '',
+    lastName: '',
+    email: '',
+    photo: '',
+    linkedIn: '',
+},
+{
+    firstName: '',
+    lastName: '',
+    email: '',
+    photo: '',
+    linkedIn: '',
+},
+{
+    firstName: '',
+    lastName: '',
+    email: '',
+    photo: '',
+    linkedIn: '',
+}];
+
+export default teamList;
