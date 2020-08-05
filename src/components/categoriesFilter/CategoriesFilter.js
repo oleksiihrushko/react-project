d1a696946d41e21b24e31011a1bc57b311a75eb8
unknown <you@example.com>
2020-08-05 10:34:46 +0300

    header1

--- src/components/categoriesFilter/CategoriesFilter.js
+++ src/components/categoriesFilter/CategoriesFilter.js
@@ -1,8 +1,22 @@
 import React from "react";
 import styles from "./CategoriesFilter.module.css";
 import config from "./config";
+import * as count from "./services";
+import { useSelector } from "react-redux";
+// import GoToMono from "./monoBank/GoToMono";
 
 const CategoriesFilter = () => {
+  // console.log("count", count.default.countCar);
+
+  // const costs = useSelector((state) => 
+  // console.log(state.operations.costs))
+  // .operations.costs.product.name 
+  // const costs = useSelector(state => {console.log('state', state.operations.costs)
+  // state.operations
+  // })
+
+  // console.log("costs", costs);
+
   return (
     <section className={`${styles.wrapper} container`}>
       <ul className={styles.ul} className={styles.flex}>
@@ -11,11 +25,11 @@ const CategoriesFilter = () => {
             <li key={name} className={`${styles.flex} ${styles.li}`}>
               <button
                 onClick={() => {
-                  console.log("123", 123);
+                  console.log("123", 123); //куда
                 }}
                 className={styles.btn}
               >
-                <p className={`${styles.prise}`}>prise</p>
+                <p className={`${styles.prise}`}>total</p>
                 <div className={styles.svg}>{svg}</div>
                 <p className={styles.name}>{name}</p>
               </button>
