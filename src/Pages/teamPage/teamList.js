9a414042156aba0d3bae655c9b523d32b87f3e9f
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2020-08-05 23:45:17 +0300

    teamPage

--- src/Pages/teamPage/teamList.js
+++ src/Pages/teamPage/teamList.js
@@ -5,80 +5,93 @@ import zavadskiy from './pageContactUs/zavadskiy.jpg';
 import jdanov from './pageContactUs/jdanov.jpg';
 import pedko from './pageContactUs/pedko.jpg';
 import reint from './pageContactUs/reint.jpg';
+import mavrodiy from './pageContactUs/mavrodiy.jpg';
+import svirko from './pageContactUs/svirko.jpg';
 // import alina from './pageContactUs/alina.jpg';
 // import alina from './pageContactUs/alina.jpg';
-// import alina from './pageContactUs/alina.jpg';
-// import alina from './pageContactUs/alina.jpg';
-const teamList = [{
+const teamList = [
+  {
+    firstName: 'Евгения',
+    lastName: 'Свирко',
+    email: '',
+    photo: svirko,
+    linkedIn: '',
+    timer: 250,
+  },
+  {
+    firstName: 'Михаил',
+    lastName: 'Мавродий',
+    email: 'mavrodiy.m.e@gmail.com',
+    photo: mavrodiy,
+    linkedIn: '',
+    timer: 200,
+  },
+  {
+    firstName: 'Алина',
+    lastName: 'Старенкова',
+    email: 'amie.affection@gmail.com',
+    photo: alina,
+    linkedIn: 'https://www.linkedin.com/in/alina-starenkova/',
+    timer: 180,
+  },
+  {
     firstName: 'Александр',
     lastName: 'Завадский',
     email: 'Zavadskiyweb@gmail.com',
     photo: zavadskiy,
     linkedIn: '',
-},
-{
+    timer: 250,
+  },
+  {
     firstName: 'Дмитрий',
     lastName: 'Смаковский',
     email: 'dmsmakovskyi@gmail.com',
     photo: smakovskyi,
-    linkedIn: '',
-},
-{
+    linkedIn: 'https://www.linkedin.com/in/dmytro-smakovskyi-5b73331b1/',
+    timer: 250,
+  },
+  {
     firstName: 'Юрий',
     lastName: 'Педько',
     email: 'yuriipedko@gmail.com',
     photo: pedko,
     linkedIn: '',
-},
-{
-    firstName: '',
-    lastName: '',
+    timer: 200,
+  },
+  {
+    firstName: 'Алексей',
+    lastName: 'Грушко',
     email: '',
     photo: hrushko,
     linkedIn: '',
-    
-},
-{
-    firstName: 'Алина',
-    lastName: '',
-    email: '',
-    photo: alina,
-    linkedIn: '',
-},
-{
+    timer: 230,
+  },
+  {
     firstName: 'Денис',
     lastName: 'Реинт',
     email: 'reintdenis@gmail.com',
     photo: reint,
-    linkedIn: 'https://www.linkedin.com/in/%D0%B4%D0%B5%D0%BD%D0%B8%D1%81-%D1%80%D0%B5%D0%B8%D0%BD%D1%82-b2a5481b4/',
-},
-{
-    firstName: '',
-    lastName: '',
+    linkedIn:
+      'https://www.linkedin.com/in/%D0%B4%D0%B5%D0%BD%D0%B8%D1%81-%D1%80%D0%B5%D0%B8%D0%BD%D1%82-b2a5481b4/',
+    timer: 200,
+  },
+  {
+    firstName: 'Женя',
+    lastName: 'Жданов',
     email: '',
     photo: jdanov,
     linkedIn: 'https://www.linkedin.com/in/yevhen-zhdanov-199923160/',
-},
-{
-    firstName: '',
-    lastName: '',
+    timer: 200,
+  },
+
+  {
+    firstName: 'Сергей',
+    lastName: 'Ковбасенко',
     email: '',
     photo: '',
     linkedIn: '',
-},
-{
-    firstName: '',
-    lastName: '',
-    email: '',
-    photo: '',
-    linkedIn: '',
-},
-{
-    firstName: '',
-    lastName: '',
-    email: '',
-    photo: '',
-    linkedIn: '',
-}];
+    timer: 200,
+  },
+];
 
 export default teamList;
