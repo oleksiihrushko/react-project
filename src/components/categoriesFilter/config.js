395c8dfc15ca558dbcae45094ef695af6a763746
Tureckiy <ev.zhdanov@gmail.com>
2020-08-06 18:38:51 +0300

    all done

--- src/components/categoriesFilter/config.js
+++ src/components/categoriesFilter/config.js
@@ -13,146 +13,165 @@ import { ReactComponent as Products } from './svg/products.svg';
 import { ReactComponent as Sport } from './svg/sport.svg';
 import { ReactComponent as Tools } from './svg/tools.svg';
 import { useSelector } from 'react-redux';
-import { makeSummary } from '../../services/helpers';
+// import { makeSummary } from '../../services/helpers';
 
-const getFilteredDate = (allCosts, monthToFilter) =>{
-  // console.log('allCosts==============', allCosts)
-  // console.log('monthToFilter==============', monthToFilter)
- const aaa = allCosts.filter(costs => {
+// const getFilteredDate = (allCosts, monthToFilter) =>{
+//   console.log('allCosts==============', allCosts)
+//   console.log('monthToFilter==============', monthToFilter)
+//  const aaa = allCosts.filter(costs => {
+//     const allDates = costs.date;
+//     const dataTarcsaction = new Date(allDates);
+//     const currentMonth = dataTarcsaction.getMonth() + 1;
+//     const currentYaer = dataTarcsaction.getFullYear();
+
+//     const monthToFindSearsh = new Date(monthToFilter);
+//     const getMonthToSearch = monthToFindSearsh.getDate();
+//     const getYaerToSearch = monthToFindSearsh.getFullYear();
+
+//     if (currentMonth === getMonthToSearch && getYaerToSearch === currentYaer) {
+//       return true;
+//     }
+//   })
+
+//   console.log('aaa==============', aaa)
+//   return aaa
+// }
+
+// const totalCategoryCost = data => {
+//   // debugger
+//   //
+//   const aaa = data.reduce(
+//     (acc, costs) => {
+//       // debugger
+//       acc[costs.product.category.name] =
+//         acc[costs.product.category.name] + costs.amount;
+//       return acc;
+//     },
+//     {
+//       Продукты: 0,
+//       Aлкоголь: 0,
+//       Транспорт: 0,
+//       Развлечение: 0,
+//       Здоровье: 0,
+//       'Все для дома': 0,
+//       Техника: 0,
+//       'Коммуналка, связь': 0,
+//       Хобби: 0,
+//       Образование: 0,
+//       Прочее: 0,
+//     },
+//   );
+
+//   console.log('aaa 2222222222222222', aaa);
+
+//   return aaa;
+// };
+
+export const TotalCountsCosts = ({ setCurrentCategory }) => {
+  const allCosts = useSelector(state => state.operations.costs);
+  // data to Filter
+  const dateToFilter = useSelector(state => state.statistics.month); ///++
+  const dateParce = dateToFilter.split('.').map(i => Number(i));
+  const monthToFilter = dateParce[1];
+  const yearToFilter = dateParce[2];
+
+  const getFilteredDate = allCosts.filter(costs => {
     const allDates = costs.date;
     const dataTarcsaction = new Date(allDates);
-    const currentMonth = dataTarcsaction.getMonth() + 1;
-    const currentYaer = dataTarcsaction.getFullYear();
+    const currentMonthTarcsaction = dataTarcsaction.getMonth() + 1;
+    const currentYaerarcsaction = dataTarcsaction.getFullYear();
 
-    const monthToFindSearsh = new Date(monthToFilter);
-    const getMonthToSearch = monthToFindSearsh.getDate();
-    const getYaerToSearch = monthToFindSearsh.getFullYear();
-
-    if (currentMonth === getMonthToSearch && getYaerToSearch === currentYaer) {
+    if (
+      monthToFilter === currentMonthTarcsaction &&
+      yearToFilter === currentYaerarcsaction
+    ) {
       return true;
     }
-  })
-
-  // console.log('aaa==============', aaa)
-  return aaa
-}
-
-const totalCategoryCost = data =>{
-  // debugger
-// 
- const aaa =  data.reduce(
+  });
+//   // console.log('Object.values total', Object.values(total))
+//   // const onShow = Object.values(total).reduce((acc, item) => {
+//   //   console.log('item', item);
+//   //   if (!isNaN(item) ) {
+//   //     acc += item;
+//   //   }
+  const totalCategoryCost = getFilteredDate.reduce(
     (acc, costs) => {
-      // debugger
       acc[costs.product.category.name] =
         acc[costs.product.category.name] + costs.amount;
       return acc;
     },
     {
       Продукты: 0,
-      Aлкоголь: 0,
+      Алкоголь: 0,
       Транспорт: 0,
       Развлечение: 0,
       Здоровье: 0,
       'Все для дома': 0,
       Техника: 0,
-      'Коммуналка, связь': 0,
+      'Коммуналка,Связь': 0,
       Хобби: 0,
       Образование: 0,
       Прочее: 0,
     },
   );
-
-console.log('aaa 11111111111111',aaa)
-
-return aaa
-}
-
-
-
-export const TotalCountsCosts = ({ setCurrentCategory }) => {
-  const allCosts = useSelector(state => state.operations.costs);
-  const monthToFilter = useSelector(state => state.statistics.month); //+
-  const total = totalCategoryCost(getFilteredDate(allCosts, monthToFilter));
-
-  // const income = useSelector((state) => state.operations.income);
-  // const costs = useSelector((state) => state.operations.costs);
-  // console.log('makeSummary()', makeSummary(costs))
-  console.log('total', total)
-  
-  // console.log('Object.values total', Object.values(total))
-  // const onShow = Object.values(total).reduce((acc, item) => {
-  //   console.log('item', item);
-  //   if (!isNaN(item) ) {
-  //     acc += item;
-  //   }
-
-  //   console.log('acc', acc);
-  // }, 0);
-
-  // console.log('onShow', onShow);
-
   const configs = [
     {
       name: 'Продукты',
       svg: <Products />,
-      total: total['Продукты'],
+      total: totalCategoryCost['Продукты'],
     },
     {
-      name: 'Aлкоголь',
+      name: 'Алкоголь',
       svg: <Alcohol />,
-      total: total['Aлкоголь'],
+      total: totalCategoryCost['Алкоголь'],
     },
     {
       name: 'Развлечение',
       svg: <Party />,
-      total: total['Развлечение'],
+      total: totalCategoryCost['Развлечение'],
     },
     {
       name: 'Здоровье',
       svg: <Health />,
-      total: total['Здоровье'],
+      total: totalCategoryCost['Здоровье'],
     },
     {
       name: 'Транспорт',
       svg: <Car />,
-      total: total['Транспорт'],
+      total: totalCategoryCost['Транспорт'],
     },
     {
       name: 'Все для дома',
       svg: <Home />,
-      total: total['Все для дома'],
+      total: totalCategoryCost['Все для дома'],
     },
     {
       name: 'Техника',
       svg: <Tools />,
-      total: total['Техника'],
+      total: totalCategoryCost['Техника'],
     },
     {
       name: 'Коммуналка, связь',
       svg: <Comunal />,
-      total: total['Коммуналка, связь'],
+      total: totalCategoryCost['Коммуналка,Связь'],
     },
     {
       name: 'Хобби',
       svg: <Sport />,
-      total: total['Хобби'],
+      total: totalCategoryCost['Хобби'],
     },
     {
       name: 'Образование',
       svg: <Learn />,
-      total: total['Образование'],
+      total: totalCategoryCost['Образование'],
     },
     {
       name: 'Прочее',
       svg: <Else />,
-      total: total['Прочее'],
-    },
-    {
-      name: 'Корректировка',
-      svg: <Else />,
-      total: total['Корректировка'],
+      total: totalCategoryCost['Прочее'],
     },
   ];
+
   return (
     <section className={`${styles.wrapper} container`}>
       <ul
@@ -163,21 +182,19 @@ export const TotalCountsCosts = ({ setCurrentCategory }) => {
       >
         {configs.map(({ name, svg, total }) => {
           return (
-            // (total !== null) > 0 && (
-            total > 0 && (
-              <li key={name} className={`${styles.flex} ${styles.li}`}>
-                <button
-                  onClick={() => {
-                    setCurrentCategory(name);
-                  }}
-                  className={styles.btn}
-                >
-                  <p className={`${styles.prise}`}>{total}</p>
-                  <div className={styles.svg}>{svg}</div>
-                  <p className={styles.name}>{name}</p>
-                </button>
-              </li>
-            )
+            // total > 0 && (
+            <li key={name} className={`${styles.flex} ${styles.li}`}>
+              <button
+                onClick={() => {
+                  setCurrentCategory(name);
+                }}
+                className={styles.btn}
+              >
+                <p className={`${styles.prise}`}>{total}</p>
+                <div className={styles.svg}>{svg}</div>
+                <p className={styles.name}>{name}</p>
+              </button>
+            </li>
           );
         })}
       </ul>
