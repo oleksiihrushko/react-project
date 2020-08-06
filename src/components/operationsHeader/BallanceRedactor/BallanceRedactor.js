ca3b51274039bfb6d81b6a8b1976ff29867a2177
Tureckiy <ev.zhdanov@gmail.com>
2020-08-06 23:40:18 +0300

    css fix

--- src/components/operationsHeader/BallanceRedactor/BallanceRedactor.js
+++ src/components/operationsHeader/BallanceRedactor/BallanceRedactor.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from "react";
-import styles from "./ballanceRedactor.module.css";
-import { useDispatch, useSelector } from "react-redux";
-import GoToStatsButton from "./GoToStatsButton/GoToStatsButton";
-import { addBalance } from "../../../redux/finance/financeOperations";
+import React, { useState, useEffect } from 'react';
+import styles from './ballanceRedactor.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import GoToStatsButton from './GoToStatsButton/GoToStatsButton';
+import { addBalance } from '../../../redux/finance/financeOperations';
 
 const BallanceRedactor = () => {
   const [isEditing, setEditing] = useState(false);
   // const [isFirstTransaction, setisFirstTransaction] = useState(true);
 
-  const balance = useSelector((state) => state.operations.balance);
+  const balance = useSelector(state => state.operations.balance);
   const [value, setValue] = useState(balance);
 
   // const income = useSelector((state) => state.operations.income);
@@ -16,23 +16,23 @@ const BallanceRedactor = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    window.addEventListener("keydown", escListener);
+    window.addEventListener('keydown', escListener);
     return () => {
-      window.removeEventListener("keydown", escListener);
+      window.removeEventListener('keydown', escListener);
     };
   }, []);
 
   const togleEdit = () => setEditing(!isEditing);
-  const escListener = (event) => {
+  const escListener = event => {
     if (event.keyCode === 27) {
       event.keyCode === 27 && setEditing(false);
-      setValue("");
+      setValue('');
     }
   };
 
   const handleChange = ({ target: { value } }) => {
-    if (value.indexOf(".") != "-1") {
-      value = value.substring(0, value.indexOf(".") + 0);
+    if (value.indexOf('.') != '-1') {
+      value = value.substring(0, value.indexOf('.') + 0);
     }
     !isNaN(value) && setValue(value);
   };
@@ -41,52 +41,56 @@ const BallanceRedactor = () => {
   //   setisFirstTransaction(false);
   // }
 
-  const handleSubmit = (event) => {
+  const handleSubmit = event => {
     event.preventDefault();
-    if (value === "") {
+    if (value === '') {
       togleEdit();
       return;
     }
     if (value !== 0) {
       const newValue = value - balance;
       dispatch(addBalance({ amount: newValue }));
-      setValue("");
+      setValue('');
       togleEdit();
     }
   };
   return (
     <section
-      className={`${styles.flex} ${styles.wrapper}  ${styles.secPad}  container`}>
+      className={`${styles.flex} ${styles.wrapper}  ${styles.secPad}  container`}
+    >
       <GoToStatsButton />
       <div className={`${styles.flex} ${styles.div} `}>
-      <p className={`${styles.bal_text}  `}>Баланс:</p>
-      <div className={`${styles.flex} ${styles.ballanceWrap}`}>
-        {isEditing ? (
-          <form onSubmit={handleSubmit} className={styles.flex}>
-            <input
-              autoFocus
-              className={`${styles.flex} ${styles.value} ${styles.inputCor}`}
-              type="text"
-              value={value}
-              onChange={handleChange}
-            />
-            <button type="submit" className={`${styles.flex} ${styles.btn}`}>
-              подтвердить
-            </button>
-          </form>
-        ) : (
-          <div className={styles.flex}>
-            <p className={styles.value}>{balance}</p>
-            <button
-              type="button"
-              className={`${styles.flex} ${styles.btn}`}
-              onClick={() => togleEdit()}
-            >
-              изменить
-            </button>
-          </div>
-        )}
-      </div>
+        <p className={`${styles.bal_text}  `}>Баланс:</p>
+        <div className={`${styles.flex} ${styles.ballanceWrap}`}>
+          {isEditing ? (
+            <form onSubmit={handleSubmit} className={styles.flex}>
+              <input
+                autoFocus
+                className={`${styles.flex} ${styles.value} ${styles.inputCor}`}
+                type="text"
+                value={value}
+                onChange={handleChange}
+              />
+              <button
+                type="submit"
+                className={`${styles.flex} ${styles.btn} ${styles.btnToSubmit}`}
+              >
+                подтвердить
+              </button>
+            </form>
+          ) : (
+            <div className={styles.flex}>
+              <p className={styles.value}>{balance}</p>
+              <button
+                type="button"
+                className={`${styles.flex} ${styles.btn}`}
+                onClick={() => togleEdit()}
+              >
+                изменить
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </section>
   );
