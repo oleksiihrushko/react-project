b337bc8610dc4fc961c31d4bdacaaecc3777c922
hrushkooleksii <hrushkooleksii@gmail.com>
2020-08-07 12:32:16 +0300

    fixing exchange

--- src/components/exchangeRates/ExchangeRates.js
+++ src/components/exchangeRates/ExchangeRates.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import { connect } from "react-redux";
-import exchangeRatesSelectors from "../../redux/exchange/exchangeRatesSelectors";
-import moment from "moment";
+import { connect } from 'react-redux';
+import exchangeRatesSelectors from '../../redux/exchange/exchangeRatesSelectors';
+import moment from 'moment';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import Table from "react-bootstrap/Table";
-import styles from "./ExchangeRates.module.css";
+import styles from './ExchangeRates.module.css';
 
 class ExchangeRates extends Component {
   state = {
-    date: "",
+    date: '',
   };
 
   componentDidMount() {
@@ -27,38 +27,37 @@ class ExchangeRates extends Component {
 
     return (
       <>
-
         <div className={`${styles.ExchangeRatesWrapper} container`}>
-          <h6>курс валют на: {date && moment(date).format("L")}</h6>
+          <h6>курс валют на: {date && moment(date).format('L')}</h6>
           {/* <Table responsive striped bordered hover variant="light"> */}
-            <caption className="text-danger">
-              <span>* курс биткоина указан в USD</span>
-            </caption>
+          <caption className="text-danger">
+            <span>* курс биткоина указан в USD</span>
+          </caption>
 
-            <thead>
+          <thead>
             <tr className={styles.tr}>
-                <th className={styles.th}>Валюта</th>
-                <th className={styles.th}>курс покупки</th>
-                <th className={styles.th}>курс продажи</th>
+              <th className={styles.th}>Валюта</th>
+              <th className={styles.th}>курс покупки</th>
+              <th className={styles.th}>курс продажи</th>
+            </tr>
+          </thead>
+          <tbody>
+            {exchangeRates.map(currency => (
+              <tr key={currency.ccy} className={styles.tr}>
+                <td className={styles.td}>{currency.ccy}</td>
+                <td className={styles.td}>{currency.buy}</td>
+                <td className={styles.td}>{currency.sale}</td>
               </tr>
-            </thead>
-            <tbody>
-              {exchangeRates.map((currency) => (
-                <tr key={currency.ccy} className={styles.tr}>
-                  <td className={styles.td}>{currency.ccy}</td>
-                  <td className={styles.td}>{currency.buy}</td>
-                  <td className={styles.td}>{currency.sale}</td>
-                </tr>
-              ))}
-            </tbody>
-          {/* </Table> */
+            ))}
+          </tbody>
+          {/* </Table> */}
         </div>
       </>
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     exchangeRates: exchangeRatesSelectors.getExchangeRates(state),
     isLoadingExhangeRates: exchangeRatesSelectors.getLoading(state),
